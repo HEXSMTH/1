7ad48d26f5fd3f5f677be50290110d0b5b370f08
HEXSMTH <hexir96@gmail.com>
2018-03-09 00:55:01 +0100

    Add files via upload

---
+++ map.js
@@ -0,0 +1,12 @@
+function initMap() {
+        var location = {lat: 40.706, lng: -74.011};
+        var map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 15,
+            center: location,
+            gestureHandling: 'greedy'
+        });
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+    }
